17be2f1c1f0405833003e0fdebb0cf7752498747
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2021-12-08 17:31:45 +0800

    0

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  // base: './',
+  base: '/gi-demo/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
