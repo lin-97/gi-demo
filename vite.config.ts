a6cdd43521d34efa566fa580290fa6479123794a
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2022-02-11 22:36:11 +0800

    打包

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: './',
+  base: '/gi-demo/',
   // 引入sass全局样式变量
   css: {
     preprocessorOptions: {
