a942d53716886c2443f6937c7bb459244087784d
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2022-03-22 14:08:18 +0800

    改为哈希路由，优化axios封装

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   {
@@ -73,7 +73,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior: () => ({ left: 0, top: 0 })
 })
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,41 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
-import { Message } from '@arco-design/web-vue'
-import { getToken, clearToken } from '@/utils/auth'
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios'
+import { Message, Notification } from '@arco-design/web-vue'
+import { getToken } from '@/utils/auth'
+import NProgress from 'nprogress'
+
+export interface BaseResponse<T = any> {
+  code: number
+  data: T
+  message: string
+  success: boolean
+}
+
+interface ICodeMessage {
+  [propName: number]: string
+}
+
+interface CustomAxiosRequestConfig extends AxiosRequestConfig {
+  hideLoading?: boolean
+}
+
+const CodeMessage: ICodeMessage = {
+  200: '服务器成功返回请求的数据。',
+  201: '新建或修改数据成功。',
+  202: '一个请求已经进入后台排队（异步任务）。',
+  204: '删除数据成功。',
+  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
+  401: '用户没有权限（令牌、用户名、密码错误）。',
+  403: '用户得到授权，但是访问是被禁止的。',
+  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
+  405: '请求方法不被允许。',
+  406: '请求的格式不可得。',
+  410: '请求的资源被永久删除，且不会再得到的。',
+  422: '当创建一个对象时，发生一个验证错误。',
+  500: '服务器发生错误，请检查服务器。',
+  502: '网关错误。',
+  503: '服务不可用，服务器暂时过载或维护。',
+  504: '网关超时。'
+}
 
 const http: AxiosInstance = axios.create({
   // baseURL: process.env.VUE_APP_API_PREFIX,
@@ -9,58 +44,68 @@ const http: AxiosInstance = axios.create({
 
 // 请求拦截器
 http.interceptors.request.use(
-  function (config: AxiosRequestConfig) {
+  (config: AxiosRequestConfig) => {
+    NProgress.start() // 进度条
+
     const token = getToken()
     if (token) {
-      // config.headers.Authorization = `${TokenPrefix}${token}`
       config.headers['token'] = token
     }
     return config
   },
-  function (error: any) {
-    return Promise.reject(error)
+  (error: { message: string }) => {
+    Message.error(error.message)
   }
 )
 
 // 响应拦截器
 http.interceptors.response.use(
-  function (response: AxiosResponse) {
-    if (response.status == 200) {
-      if (response.data.success) {
-        return response.data
-      } else {
-        Message.clear()
-        Message.error({ content: response.data.message || response.data.msg || '服务器端错误' })
-        return Promise.reject(response.data)
-      }
+  (response: AxiosResponse) => {
+    const { data } = response
+    const { msg, message, success } = data
+
+    if (!success) {
+      Notification.error(message || msg || '服务器端错误')
+      return Promise.reject(new Error(msg || 'Error'))
     }
+
+    NProgress.done()
+
+    return data
   },
-  function (error) {
-    let message = error.message
-    if (error.response.status === 404) {
-      message = '接口访问失败 404'
-    }
-    Message.error({ content: message })
+  (error: AxiosError) => {
+    NProgress.done()
+    Message.clear()
+
+    const response = Object.assign({}, error.response)
+    response && Message.error(CodeMessage[response.status] || '系统异常')
     return Promise.reject(error)
   }
 )
 
-export default {
-  post(url: string, data: object, headers = {}) {
-    return http({
-      method: 'post',
-      url,
-      data,
-      headers
-    })
-  },
-
-  get(url: string, params: object, headers = {}) {
-    return http({
-      method: 'get',
-      url,
-      params,
-      headers
-    })
-  }
+const request = <T = any>(config: CustomAxiosRequestConfig): Promise<T> => {
+  return new Promise((resolve, reject) => {
+    http
+      .request<BaseResponse<T>>(config)
+      .then((res: AxiosResponse) => resolve(res.data))
+      .catch((err: { message: string }) => reject(err))
+  })
 }
+
+request.get = <T = any>(url: string, params?: object): Promise<T> => {
+  return request({
+    method: 'get',
+    url,
+    params
+  })
+}
+
+request.post = <T = any>(url: string, params?: object): Promise<T> => {
+  return request({
+    method: 'post',
+    url,
+    data: params
+  })
+}
+
+export default request
