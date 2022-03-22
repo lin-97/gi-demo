6af0963a1898129b6ca5c0a592ab82b235941404
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2022-03-22 21:31:01 +0800

    打包

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import router from './router'
 
 // 引入 Arco Design 组件库以及自定义主题
 import ArcoVue from '@arco-design/web-vue'
-import '@arco-themes/vue-demo/index.less'
+import '@arco-themes/vue-gi-demo/index.less'
 // import '@arco-design/web-vue/dist/arco.css'
 
 // 额外引入 Arco Design Icon图标库
