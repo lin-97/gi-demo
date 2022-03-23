70e99abe342b794c6673b6582e3c8a7c30e0413b
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2022-03-23 16:31:50 +0800

    可拖拽音频组件

--- src/views/components/ThePreviewAudio/Dialog.vue
+++ src/views/components/ThePreviewAudio/Dialog.vue
@@ -1,14 +1,27 @@
 <template>
-  <a-modal width="auto" v-model:visible="visible" @cancel="handleCancel">
-    <canvas height="110" style="width: 100%"></canvas>
-    <div id="audio"></div>
-  </a-modal>
+  <transition name="slide-dynamic-origin">
+    <div class="audio-preview-wrapper" ref="audioRef" :style="audioStyle" style="position: fixed" v-show="visible">
+      <section style="padding: 10px 14px 14px 14px">
+        <div class="head" ref="audioHeadRef">
+          <div class="name">
+            <icon-music :size="16" spin />
+            <span>{{ props.fileInfo?.name }}.{{ props.fileInfo?.extendName }}</span>
+          </div>
+          <div class="close-icon" @click="handleClose">
+            <icon-close :size="12" />
+          </div>
+        </div>
+
+        <!-- 音频组件 -->
+        <audio class="audio" :src="props.fileInfo?.src" controls autoplay></audio>
+      </section>
+    </div>
+  </transition>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount } from 'vue'
-import 'xgplayer'
-import Music from 'xgplayer-music'
+import { ref, onMounted, computed } from 'vue'
+import { useDraggable, useWindowSize, useElementSize } from '@vueuse/core'
 
 const props = defineProps({
   fileInfo: Object,
@@ -16,43 +29,107 @@ const props = defineProps({
 })
 
 let visible = ref<boolean>(false)
-let player = ref<any>(null)
+
+const audioRef = ref<HTMLElement | null>(null)
+const audioHeadRef = ref<HTMLElement | null>(null)
 
 onMounted(() => {
   visible.value = true
-  player.value = new Music({
-    id: 'audio',
-    url: [
-      {
-        src: props.fileInfo?.src,
-        name: props.fileInfo?.name,
-        poster: '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/music/poster-small.jpeg'
-      }
-    ],
-    volume: 0.8,
-    width: 900,
-    height: 50,
-    preloadNext: true,
-    switchKeepProgress: false,
-    volumeShow: true,
-    autoplay: true
-  })
-
-  player.value.crossOrigin = 'anonymous'
-  player.value.analyze(document.querySelector('canvas'))
 })
 
-onBeforeUnmount(() => {
-  player.value = null
+const windowSize = useWindowSize()
+const elSize = useElementSize(audioRef)
+const { x, y } = useDraggable(audioRef, {
+  initialValue: { x: windowSize.width.value - elSize.width.value, y: 40 }
 })
 
-const handleCancel = () => {
+const audioStyle = computed(() => {
+  let left: number | string = x.value + 'px'
+  let top: number | string = y.value + 'px'
+  if (x.value > windowSize.width.value - elSize.width.value) {
+    left = windowSize.width.value - elSize.width.value + 'px'
+  }
+  if (x.value < 0) {
+    left = 0
+  }
+  if (y.value > windowSize.height.value - elSize.height.value) {
+    top = windowSize.height.value - elSize.height.value
+  }
+  if (y.value < 0) {
+    top = 0
+  }
+  return {
+    left: left,
+    top: top
+  }
+})
+
+const handleClose = () => {
   visible.value = false
   props.onCancel()
 }
 
 defineExpose({
   visible,
-  handleCancel
+  handleClose
 })
 </script>
+
+<style lang="scss" scoped>
+.audio-preview-wrapper {
+  width: 300px;
+  position: fixed;
+  top: 120px;
+  right: 0;
+  border-radius: 8px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  background: linear-gradient(to right, $theme-color, rgb(var(--primary-2)));
+  z-index: 9999;
+  .head {
+    color: #fff;
+    font-size: 16px;
+    margin-bottom: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: move;
+    user-select: none;
+    &:active {
+      cursor: move;
+    }
+    .name {
+      display: flex;
+      align-items: center;
+      > span {
+        margin-left: 8px;
+      }
+    }
+    .close-icon {
+      width: 24px;
+      height: 24px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 50%;
+      background: rgba(0, 0, 0, 0);
+      transition: all 0.2s;
+      cursor: pointer;
+      svg {
+        transition: all 0.2s;
+      }
+      &:hover {
+        background: rgba(0, 0, 0, 0.1);
+        svg {
+          transform: scale(1.3);
+        }
+      }
+    }
+  }
+  .audio {
+    width: 100%;
+    &::-webkit-media-controls-enclosure {
+      background: #fff;
+    }
+  }
+}
+</style>
