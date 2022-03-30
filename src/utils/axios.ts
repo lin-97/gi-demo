7f09454aedc3657a0fa07ce98c5a44be1beea07c
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2022-03-30 15:31:51 +0800

    优化axios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -10,7 +10,7 @@ export interface BaseResponse<T = any> {
   success: boolean
 }
 
-interface ICodeMessage {
+interface CodeMessage {
   [propName: number]: string
 }
 
@@ -18,23 +18,21 @@ interface CustomAxiosRequestConfig extends AxiosRequestConfig {
   hideLoading?: boolean
 }
 
-const CodeMessage: ICodeMessage = {
-  200: '服务器成功返回请求的数据。',
+const StatusCodeMessage: CodeMessage = {
+  200: '服务器成功返回请求的数据',
   201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  405: '请求方法不被允许。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。'
+  202: '一个请求已经进入后台排队（异步任务）',
+  204: '删除数据成功',
+  400: '请求错误(400)',
+  401: '未授权，请重新登录(401)',
+  403: '拒绝访问(403)',
+  404: '请求出错(404)',
+  408: '请求超时(408)',
+  500: '服务器错误(500)',
+  501: '服务未实现(501)',
+  502: '网络错误(502)',
+  503: '服务不可用(503)',
+  504: '网络超时(504)'
 }
 
 const http: AxiosInstance = axios.create({
@@ -78,7 +76,7 @@ http.interceptors.response.use(
     Message.clear()
 
     const response = Object.assign({}, error.response)
-    response && Message.error(CodeMessage[response.status] || '系统异常')
+    response && Message.error(StatusCodeMessage[response.status] || '系统异常, 请检查网络或联系管理员！')
     return Promise.reject(error)
   }
 )
