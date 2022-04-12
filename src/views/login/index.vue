cd61d1ad55333c64af4b46ab65b5fcdeeed866ea
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2022-04-12 18:00:57 +0800

    0

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -36,6 +36,11 @@
         </a-form>
       </div>
     </section>
+
+    <div class="fly bg-fly-circle1"></div>
+    <div class="fly bg-fly-circle2"></div>
+    <div class="fly bg-fly-circle3"></div>
+    <div class="fly bg-fly-circle4"></div>
   </div>
 </template>
 
@@ -85,13 +90,12 @@ const login = () => {
   justify-content: center;
   align-items: center;
   // background-color: var(--color-bg-2);
-  background-image: url('@/assets/svgs/login-bg.svg');
   &-box {
     width: 720px;
     height: 380px;
     background: #fff;
     display: flex;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.06);
+    // box-shadow: 0 2px 4px 2px rgba(0, 0, 0, 0.06);
   }
 }
 
@@ -167,6 +171,65 @@ const login = () => {
   box-sizing: border-box;
   :deep(.arco-input-wrapper) {
     border-color: var(--color-border-2);
+  }
+}
+
+.fly {
+  pointer-events: none;
+  position: fixed;
+  z-index: 9999;
+}
+.bg-fly-circle1 {
+  left: 40px;
+  top: 100px;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background: linear-gradient(to right, rgba(100, 84, 239, 0.07) 0%, rgba(48, 33, 236, 0.04) 100%);
+  animation: move 2.5s linear infinite;
+}
+
+.bg-fly-circle2 {
+  left: 3%;
+  top: 60%;
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  background: linear-gradient(to right, rgba(100, 84, 239, 0.08) 0%, rgba(48, 33, 236, 0.04) 100%);
+  animation: move 3s linear infinite;
+}
+
+.bg-fly-circle3 {
+  right: 2%;
+  top: 140px;
+  width: 145px;
+  height: 145px;
+  border-radius: 50%;
+  background: linear-gradient(to right, rgba(100, 84, 239, 0.1) 0%, rgba(48, 33, 236, 0.04) 100%);
+  animation: move 2.5s linear infinite;
+}
+
+.bg-fly-circle4 {
+  right: 5%;
+  top: 60%;
+  width: 160px;
+  height: 160px;
+  border-radius: 50%;
+  background: linear-gradient(to right, rgba(100, 84, 239, 0.02) 0%, rgba(48, 33, 236, 0.04) 100%);
+  animation: move 3.5s linear infinite;
+}
+
+@keyframes move {
+  0% {
+    transform: translateY(0px) scale(1);
+  }
+
+  50% {
+    transform: translateY(25px) scale(1.1);
+  }
+
+  100% {
+    transform: translateY(0px) scale(1);
   }
 }
 </style>
