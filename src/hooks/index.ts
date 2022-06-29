1406b91a7213a271c8bca3ea7ba2b914f90516e1
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2022-06-29 17:14:01 +0800

    新增分页hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 import useFullScreen from './modules/useFullScreen'
 import useContextMenu from './modules/useContextMenu'
+import usePagination from './modules/usePagination'
 import useArea from './modules/useArea'
 
-export { useFullScreen, useContextMenu, useArea }
+export { useFullScreen, useContextMenu, usePagination, useArea }
---
+++ src/hooks/modules/usePagination.ts
@@ -0,0 +1,33 @@
+import { ref } from 'vue'
+
+type Callback = () => void
+
+export default function usePagination(callback: Callback, defaultPageSize = 10) {
+  const current = ref(1)
+  const pageSize = ref(defaultPageSize)
+  const total = ref(0)
+
+  function changeCurrent(size: number) {
+    current.value = size
+    callback()
+  }
+
+  function changePageSize(size: number) {
+    current.value = 1
+    pageSize.value = size
+    callback()
+  }
+
+  function setTotal(value: number) {
+    total.value = value
+  }
+
+  return {
+    current,
+    pageSize,
+    total,
+    changeCurrent,
+    changePageSize,
+    setTotal
+  }
+}
