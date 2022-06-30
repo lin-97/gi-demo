b07b57535cf7b0cc3e14af43a1489e3911c2c18e
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2022-06-30 18:16:53 +0800

    图片预览hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,5 +2,6 @@ import useFullScreen from './modules/useFullScreen'
 import useContextMenu from './modules/useContextMenu'
 import usePagination from './modules/usePagination'
 import useArea from './modules/useArea'
+import useImage from './modules/useImage'
 
-export { useFullScreen, useContextMenu, usePagination, useArea }
+export { useFullScreen, useContextMenu, usePagination, useArea, useImage }
---
+++ src/hooks/modules/useImage.ts
@@ -0,0 +1,13 @@
+import { api as viewerApi } from 'v-viewer'
+import 'viewerjs/dist/viewer.css'
+
+export default function useImage() {
+  function previewImage(src: string) {
+    viewerApi({
+      images: [src]
+    })
+  }
+  return {
+    previewImage
+  }
+}
