e078e4e66961688a6c5ef32e92167ac70ec20975
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2022-07-14 10:28:55 +0800

    0

--- dist/assets/system.0715b550.js
+++
@@ -1,1 +0,0 @@
-import{eH as t}from"./index.6689af13.js";const e="/mock";function r(){return t.get(`${e}/system/dept/list`)}function i(){return t.get(`${e}/system/user/list`,data)}function n(){return t.get(`${e}/system/role/list`,data)}export{i as a,n as b,r as g};
---
+++ dist/assets/table.ab29bea3.js
@@ -0,0 +1,1 @@
+import{eG as e}from"./index.855e9ebd.js";const r="/mock";function s(t){return e.get(`${r}/table/list`,t)}export{s as g};
