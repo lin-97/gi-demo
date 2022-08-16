541d17f3aee29886e4320155c6e004bacbc3fd47
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2022-08-16 09:26:40 +0800

    更新

---
+++ dist/assets/index.82400d26.js
@@ -0,0 +1,1 @@
+import{eC as e}from"./index.280a67e2.js";const r="/mock";function s(t){return e.get(`${r}/table/list`,t)}export{s as g};
--- dist/assets/index.b2cec821.js
+++
@@ -1,1 +0,0 @@
-import{eC as e}from"./index.ec8d6b95.js";const r="/mock";function s(t){return e.get(`${r}/table/list`,t)}export{s as g};
