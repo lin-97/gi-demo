3b9c641afde5145d9fe703de2535d84c8dcfa724
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2022-09-19 18:23:30 +0800

    更新

---
+++ dist/assets/index.246c5bd9.js
@@ -0,0 +1,1 @@
+import{ew as e}from"./index.3964aeb2.js";const r="/mock";function s(t){return e.get(`${r}/table/list`,t)}export{s as g};
---
+++ dist/assets/index.a5856132.js
@@ -0,0 +1,1 @@
+import{ew as t}from"./index.3964aeb2.js";const e="/mock";function r(){return t.get(`${e}/system/dept/list`)}function i(){return t.get(`${e}/system/user/list`)}function n(){return t.get(`${e}/system/role/list`)}export{i as a,n as b,r as g};
