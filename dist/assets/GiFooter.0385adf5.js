c10d2a63a8377c6e545c4859203ea87fd39ff1dd
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2022-09-20 17:50:08 +0800

    更新

---
+++ dist/assets/GiFooter.0385adf5.js
@@ -0,0 +1,1 @@
+import{_ as o,o as e,c as _}from"./index.9df7633a.js";const t={name:"GiFooter"};const c={class:"gi-footer"};function n(s,r,a,i,p,d){return e(),_("div",c,"Copyright 2022 Gi Admin Pro")}const m=o(t,[["render",n],["__scopeId","data-v-9673c888"]]);export{m as _};
--- dist/assets/index.246c5bd9.js
+++
@@ -1,1 +0,0 @@
-import{ew as e}from"./index.3964aeb2.js";const r="/mock";function s(t){return e.get(`${r}/table/list`,t)}export{s as g};
