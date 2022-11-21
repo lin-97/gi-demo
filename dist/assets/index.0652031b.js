1cfbe5eee2bcab9c9438f399e21358915cfa15a6
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2022-11-21 15:56:18 +0800

    更新

--- dist/assets/index.0652031b.js
+++
@@ -1,1 +0,0 @@
-import{ey as t}from"./index.2b0d088b.js";const r="/mock";function s(e){return t.get(`${r}/person/list`,e)}export{s as g};
---
+++ dist/assets/index.67a93af4.js
@@ -0,0 +1,1 @@
+import{ey as t}from"./index.48554e72.js";const r="/mock";function s(e){return t.get(`${r}/person/list`,e)}export{s as g};
