1123f69d474f2e3e2537a19e9421723c05426d3e
unknown <326010228@qq.com>
2023-05-20 21:34:52 +0800

    优化

--- src/components/ParentView/index.vue
+++ src/components/ParentView/index.vue
@@ -1,3 +1,20 @@
-<template >
-  <router-view />
+<template>
+  <div>
+    <router-view v-slot="{ Component, route }">
+      <transition :name="appStore.transitionName" mode="out-in" appear>
+        <keep-alive :include="(tabsStore.cacheList as string[])">
+          <component :is="Component" :key="route.path" />
+        </keep-alive>
+      </transition>
+    </router-view>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { useTabsStore, useAppStore } from '@/store'
+
+const tabsStore = useTabsStore()
+const appStore = useAppStore()
+</script>
+
+<style lang="scss" scoped></style>
