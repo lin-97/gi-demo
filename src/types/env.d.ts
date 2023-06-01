ee93f7687f07a7409a8946dc9d4a83caee134de7
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2023-06-01 13:03:39 +0800

    打包配置优化

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,9 +1,12 @@
-// 环境变量 TypeScript的智能提示
-interface ImportMetaEnv {
-  VITE_API_PREFIX: string;
-  VITE_API_BASE_URL: string;
+/// <reference types="vite/client" />
+
+/** 声明环境变量的类型 */
+declare interface ImportMetaEnv {
+  VITE_API_PREFIX: string
+  VITE_API_BASE_URL: string
+  VITE_PUBLIC_PATH: string
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
-import { fileURLToPath, URL } from 'url'
-
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import { fileURLToPath, URL } from 'url'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import { viteMockServe } from 'vite-plugin-mock'
-
 import path from 'path'
 
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd()) as ImportMetaEnv
+
   return {
     plugins: [
       vue(),
@@ -51,7 +50,7 @@ export default defineConfig(({ command, mode }) => {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
-    base: '/gi-demo/',
+    base: env.VITE_PUBLIC_PATH,
     // 引入sass全局样式变量
     css: {
       preprocessorOptions: {
@@ -73,11 +72,17 @@ export default defineConfig(({ command, mode }) => {
     // 构建
     build: {
       outDir: 'dist', // 指定打包路径，默认为项目根目录下的 dist 目录
+      // Vite 2.6.x 以上需要配置 minify: "terser", terserOptions 才能生效
+      minify: 'terser',
       terserOptions: {
         compress: {
           keep_infinity: true, // 防止 Infinity 被压缩成 1/0，这可能会导致 Chrome 上的性能问题
           drop_console: true, // 生产环境去除 console
           drop_debugger: true // 生产环境去除 debugger
+        },
+        format: {
+          // 删除注释
+          comments: false
         }
       },
       chunkSizeWarningLimit: 1500 // chunk 大小警告的限制（以 kbs 为单位）
