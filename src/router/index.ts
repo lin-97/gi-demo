05638a2085c17edb0482329f38a13222e5bfc7cc
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2023-07-04 10:12:07 +0800

    修复切换角色，路由没清除问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,8 +19,9 @@ export function resetRouter() {
   // 注意：所有动态路由路由必须带有 Name 属性，否则可能会不能完全重置干净
   try {
     router.getRoutes().forEach((route) => {
-      const { name, meta } = route
-      if (name && meta.roles?.length) {
+      const { name, meta, path } = route
+      // console.log('name', name, path)
+      if (name && name !== 'Home') {
         router.hasRoute(name) && router.removeRoute(name)
       }
     })
