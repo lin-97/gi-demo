d8460f18fe4ddf4a45082920b1ddbf3459c57ba9
unknown <326010228@qq.com>
2023-09-26 23:34:28 +0800

    详情页的示例优化

--- src/views/detail/base/index.vue
+++ src/views/detail/base/index.vue
@@ -1,31 +1,30 @@
 <template>
-  <div class="detail" ref="containerRef">
-    <a-affix :target="(containerRef as HTMLElement)">
-      <a-page-header title="详情" subtitle="Detail Page" class="head">
-        <template #extra>
-          <a-button>返回</a-button>
-        </template>
-      </a-page-header>
-    </a-affix>
-
+  <div class="detail">
     <a-card title="基本信息" :bordered="false" class="item">
-      <a-descriptions table-layout="fixed" :label-style="{ width: '60px' }">
-        <a-descriptions-item label="姓名">Lin</a-descriptions-item>
-        <a-descriptions-item label="性别">男</a-descriptions-item>
-        <a-descriptions-item label="生日">07月16日</a-descriptions-item>
-        <a-descriptions-item label="城市">广州市</a-descriptions-item>
-        <a-descriptions-item label="手机号">155 **** 8810</a-descriptions-item>
-        <a-descriptions-item label="邮箱">326010228@qq.com</a-descriptions-item>
-        <a-descriptions-item label="星座">双鱼座</a-descriptions-item>
-        <a-descriptions-item label="爱好">
-          <a-space :size="5">
+      <template #extra>
+        <a-button>返回</a-button>
+      </template>
+      <a-descriptions
+        table-layout="fixed"
+        layout="inline-horizontal"
+        :column="{ xs: 1, sm: 2, md: 2, lg: 2, xl: 3, xxl: 3 }"
+      >
+        <a-descriptions-item label="姓名：">Lin</a-descriptions-item>
+        <a-descriptions-item label="性别：">男</a-descriptions-item>
+        <a-descriptions-item label="生日：">07月16日</a-descriptions-item>
+        <a-descriptions-item label="城市：">广州市</a-descriptions-item>
+        <a-descriptions-item label="手机号：">155 **** 8810</a-descriptions-item>
+        <a-descriptions-item label="邮箱：">326010228@qq.com</a-descriptions-item>
+        <a-descriptions-item label="星座：">双鱼座</a-descriptions-item>
+        <a-descriptions-item label="爱好：">
+          <a-space :size="5" wrap>
             <a-tag color="purple">王者荣耀</a-tag>
             <a-tag color="magenta">打疫苗</a-tag>
           </a-space>
         </a-descriptions-item>
-        <a-descriptions-item label="座右铭">哈哈哈</a-descriptions-item>
-        <a-descriptions-item label="标签">
-          <a-space :size="5">
+        <a-descriptions-item label="座右铭：">哈哈哈</a-descriptions-item>
+        <a-descriptions-item label="标签：">
+          <a-space :size="5" wrap>
             <a-tag color="#f53f3f">vue3</a-tag>
             <a-tag color="#7816ff">pinia</a-tag>
             <a-tag color="#00b42a">vite</a-tag>
@@ -46,7 +45,7 @@
         <a-descriptions-item label="邮箱">326010228@qq.com</a-descriptions-item>
         <a-descriptions-item label="星座">双鱼座</a-descriptions-item>
         <a-descriptions-item label="爱好">
-          <a-space :size="5">
+          <a-space :size="5" wrap>
             <a-tag color="purple">王者荣耀</a-tag>
             <a-tag color="magenta">打疫苗</a-tag>
           </a-space>
@@ -54,12 +53,12 @@
         <a-descriptions-item label="座右铭">哈哈哈</a-descriptions-item>
         <a-descriptions-item label="图像">
           <img
-            style="width: 400px"
+            style="width: 100%; max-width: 400px"
             src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2F09fc753c76d4a8575c105452c81b76ba563c0d8d.jpg&refer=http%3A%2F%2Fi0.hdslb.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1652531003&t=100230590302237a20f8e136de146f6b"
           />
         </a-descriptions-item>
         <a-descriptions-item label="标签">
-          <a-space :size="5">
+          <a-space :size="5" wrap>
             <a-tag color="#f53f3f">vue3</a-tag>
             <a-tag color="#7816ff">pinia</a-tag>
             <a-tag color="#00b42a">vite</a-tag>
@@ -74,7 +73,6 @@
 
 <script setup lang="ts">
 defineOptions({ name: 'BaseDetail' })
-const containerRef = ref<HTMLElement | null>()
 </script>
 
 <style lang="scss" scoped>
--- src/views/detail/senior/index.vue
+++ src/views/detail/senior/index.vue
@@ -9,23 +9,27 @@
     </a-affix>
 
     <a-card title="基本信息" :bordered="false" class="item">
-      <a-descriptions table-layout="fixed" :label-style="{ width: '60px' }">
-        <a-descriptions-item label="姓名:">Lin</a-descriptions-item>
-        <a-descriptions-item label="性别:">男</a-descriptions-item>
-        <a-descriptions-item label="生日:">07月16日</a-descriptions-item>
-        <a-descriptions-item label="城市:">广州市</a-descriptions-item>
-        <a-descriptions-item label="手机号:">155 **** 8810</a-descriptions-item>
-        <a-descriptions-item label="邮箱:">326010228@qq.com</a-descriptions-item>
-        <a-descriptions-item label="星座:">双鱼座</a-descriptions-item>
-        <a-descriptions-item label="爱好:">
-          <a-space :size="5">
+      <a-descriptions
+        table-layout="fixed"
+        :label-style="{ width: '60px' }"
+        :column="{ xs: 1, sm: 2, md: 2, lg: 2, xl: 3, xxl: 3 }"
+      >
+        <a-descriptions-item label="姓名">Lin</a-descriptions-item>
+        <a-descriptions-item label="性别">男</a-descriptions-item>
+        <a-descriptions-item label="生日">07月16日</a-descriptions-item>
+        <a-descriptions-item label="城市">广州市</a-descriptions-item>
+        <a-descriptions-item label="手机号">155 **** 8810</a-descriptions-item>
+        <a-descriptions-item label="邮箱">326010228@qq.com</a-descriptions-item>
+        <a-descriptions-item label="星座">双鱼座</a-descriptions-item>
+        <a-descriptions-item label="爱好">
+          <a-space :size="5" wrap>
             <a-tag color="purple">王者荣耀</a-tag>
             <a-tag color="magenta">打疫苗</a-tag>
           </a-space>
         </a-descriptions-item>
-        <a-descriptions-item label="座右铭:">哈哈哈</a-descriptions-item>
-        <a-descriptions-item label="标签:">
-          <a-space :size="5">
+        <a-descriptions-item label="座右铭">哈哈哈</a-descriptions-item>
+        <a-descriptions-item label="标签">
+          <a-space :size="5" wrap>
             <a-tag color="#f53f3f">vue3</a-tag>
             <a-tag color="#7816ff">pinia</a-tag>
             <a-tag color="#00b42a">vite</a-tag>
@@ -38,40 +42,48 @@
 
     <a-card title="基本信息" :bordered="false" class="item">
       <a-row justify="space-between">
-        <a-steps :current="2" direction="vertical">
-          <a-step description="申请人:Lin">申请</a-step>
-          <a-step description="审批人:尤大大">
-            <span>领导审批</span>
-            <template #icon><GiDot></GiDot></template>
-          </a-step>
-          <a-step description="Echo">财务审批</a-step>
-        </a-steps>
+        <a-col :xs="24" :sm="8" :md="8" :lg="6" :xl="6" :xxl="6" style="margin-bottom: 20px">
+          <a-steps :current="2" direction="vertical">
+            <a-step description="申请人:Lin">申请</a-step>
+            <a-step description="审批人:尤大大">
+              <span>领导审批</span>
+              <template #icon><GiDot></GiDot></template>
+            </a-step>
+            <a-step description="Echo">财务审批</a-step>
+          </a-steps>
+        </a-col>
 
-        <a-descriptions style="flex: 1; margin-left: 20%" table-layout="fixed" :label-style="{ width: '60px' }">
-          <a-descriptions-item label="姓名:">Lin</a-descriptions-item>
-          <a-descriptions-item label="性别:">男</a-descriptions-item>
-          <a-descriptions-item label="生日:">07月16日</a-descriptions-item>
-          <a-descriptions-item label="城市:">广州市</a-descriptions-item>
-          <a-descriptions-item label="手机号:">155 **** 8810</a-descriptions-item>
-          <a-descriptions-item label="邮箱:">326010228@qq.com</a-descriptions-item>
-          <a-descriptions-item label="星座:">双鱼座</a-descriptions-item>
-          <a-descriptions-item label="爱好:">
-            <a-space :size="5">
-              <a-tag color="purple">王者荣耀</a-tag>
-              <a-tag color="magenta">打疫苗</a-tag>
-            </a-space>
-          </a-descriptions-item>
-          <a-descriptions-item label="座右铭:">哈哈哈</a-descriptions-item>
-          <a-descriptions-item label="标签:">
-            <a-space :size="5">
-              <a-tag color="#f53f3f">vue3</a-tag>
-              <a-tag color="#7816ff">pinia</a-tag>
-              <a-tag color="#00b42a">vite</a-tag>
-              <a-tag color="#165dff">ts</a-tag>
-              <a-tag color="#ff7d00">arco design</a-tag>
-            </a-space>
-          </a-descriptions-item>
-        </a-descriptions>
+        <a-col :xs="24" :sm="16" :md="16" :lg="18" :xl="18" :xxl="18">
+          <a-descriptions
+            table-layout="fixed"
+            layout="inline-horizontal"
+            :column="{ xs: 1, sm: 2, md: 2, lg: 2, xl: 3, xxl: 3 }"
+          >
+            <a-descriptions-item label="姓名：">Lin</a-descriptions-item>
+            <a-descriptions-item label="性别：">男</a-descriptions-item>
+            <a-descriptions-item label="生日：">07月16日</a-descriptions-item>
+            <a-descriptions-item label="城市：">广州市</a-descriptions-item>
+            <a-descriptions-item label="手机号：">155 **** 8810</a-descriptions-item>
+            <a-descriptions-item label="邮箱：">326010228@qq.com</a-descriptions-item>
+            <a-descriptions-item label="星座：">双鱼座</a-descriptions-item>
+            <a-descriptions-item label="爱好：">
+              <a-space :size="5" wrap>
+                <a-tag color="purple">王者荣耀</a-tag>
+                <a-tag color="magenta">打疫苗</a-tag>
+              </a-space>
+            </a-descriptions-item>
+            <a-descriptions-item label="座右铭：">哈哈哈</a-descriptions-item>
+            <a-descriptions-item label="标签：">
+              <a-space :size="5" wrap>
+                <a-tag color="#f53f3f">vue3</a-tag>
+                <a-tag color="#7816ff">pinia</a-tag>
+                <a-tag color="#00b42a">vite</a-tag>
+                <a-tag color="#165dff">ts</a-tag>
+                <a-tag color="#ff7d00">arco design</a-tag>
+              </a-space>
+            </a-descriptions-item>
+          </a-descriptions>
+        </a-col>
       </a-row>
     </a-card>
 
@@ -88,28 +100,28 @@
 
     <a-card title="其他信息" :bordered="false" class="item">
       <a-descriptions :column="1">
-        <a-descriptions-item label="姓名:">Lin</a-descriptions-item>
-        <a-descriptions-item label="性别:">男</a-descriptions-item>
-        <a-descriptions-item label="生日:">07月16日</a-descriptions-item>
-        <a-descriptions-item label="城市:">广州市</a-descriptions-item>
-        <a-descriptions-item label="手机号:">155 **** 8810</a-descriptions-item>
-        <a-descriptions-item label="邮箱:">326010228@qq.com</a-descriptions-item>
-        <a-descriptions-item label="星座:">双鱼座</a-descriptions-item>
-        <a-descriptions-item label="爱好:">
+        <a-descriptions-item label="姓名：">Lin</a-descriptions-item>
+        <a-descriptions-item label="性别：">男</a-descriptions-item>
+        <a-descriptions-item label="生日：">07月16日</a-descriptions-item>
+        <a-descriptions-item label="城市：">广州市</a-descriptions-item>
+        <a-descriptions-item label="手机号：">155 **** 8810</a-descriptions-item>
+        <a-descriptions-item label="邮箱：">326010228@qq.com</a-descriptions-item>
+        <a-descriptions-item label="星座：">双鱼座</a-descriptions-item>
+        <a-descriptions-item label="爱好：">
           <a-space :size="5">
             <a-tag color="purple">王者荣耀</a-tag>
             <a-tag color="magenta">打疫苗</a-tag>
           </a-space>
         </a-descriptions-item>
-        <a-descriptions-item label="座右铭:">哈哈哈</a-descriptions-item>
-        <a-descriptions-item label="图像:">
+        <a-descriptions-item label="座右铭：">哈哈哈</a-descriptions-item>
+        <a-descriptions-item label="图像：">
           <img
-            style="width: 400px"
+            style="width: 100%; max-width: 400px"
             src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2F09fc753c76d4a8575c105452c81b76ba563c0d8d.jpg&refer=http%3A%2F%2Fi0.hdslb.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1652531003&t=100230590302237a20f8e136de146f6b"
           />
         </a-descriptions-item>
-        <a-descriptions-item label="标签:">
-          <a-space :size="5">
+        <a-descriptions-item label="标签：">
+          <a-space :size="5" wrap>
             <a-tag color="#f53f3f">vue3</a-tag>
             <a-tag color="#7816ff">pinia</a-tag>
             <a-tag color="#00b42a">vite</a-tag>
