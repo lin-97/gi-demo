0ce2b7529c7fb1eb566f8d1fe60140aae1f0d6e5
史蒂芬林 <53759423+lin-97@users.noreply.github.com>
2023-10-13 15:10:12 +0800

    首页项目卡片响应式优化

--- src/views/home/components/ProjectCard.vue
+++ src/views/home/components/ProjectCard.vue
@@ -1,19 +1,23 @@
 <template>
   <a-card title="项目" :bordered="false" size="medium" style="overflow: hidden">
-    <a-card-grid :style="{ width: '33.33%' }" v-for="(item, index) in list" :key="item.name" class="card-grid-item">
-      <a-card :bordered="false" hoverable :class="'animated-fade-up-' + index">
-        <section class="item">
-          <div class="head">
-            <GiSvgIcon :size="30" :name="item.icon"></GiSvgIcon>
-            <span class="name gi_line_1">{{ item.name }}</span>
-          </div>
-          <div class="middle">
-            <p class="desc gi_line_2">{{ item.desc }}</p>
-          </div>
-          <p class="foot gi_line_1">{{ item.source }} {{ Dayjs(new Date()).format('YYYY-MM-DD') }}</p>
-        </section>
-      </a-card>
-    </a-card-grid>
+    <a-row align="stretch">
+      <a-col :xs="12" :sm="8" :md="8" v-for="(item, index) in list" :key="item.name">
+        <a-card-grid class="w-full h-full">
+          <a-card :bordered="false" hoverable :class="'animated-fade-up-' + index">
+            <section class="item">
+              <div class="head">
+                <GiSvgIcon :size="30" :name="item.icon"></GiSvgIcon>
+                <span class="name gi_line_1">{{ item.name }}</span>
+              </div>
+              <div class="middle">
+                <p class="desc gi_line_2">{{ item.desc }}</p>
+              </div>
+              <p class="foot gi_line_1">{{ item.source }} {{ Dayjs(new Date()).format('YYYY-MM-DD') }}</p>
+            </section>
+          </a-card>
+        </a-card-grid>
+      </a-col>
+    </a-row>
   </a-card>
 </template>
 
