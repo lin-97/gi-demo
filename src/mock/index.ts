f2697490a45ab57c254b67c1e376ef4a9fc6ecee
unknown <326010228@qq.com>
2023-10-31 23:03:47 +0800

    mock添加area模块

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -6,7 +6,16 @@ import systemModule from './system/index'
 import fileModule from './file/index'
 import testModule from './test/index'
 import cateModule from './cate/index'
+import areaModule from './area/index'
 
 export function setupProdMockServer() {
-  createProdMockServer([...userModule, ...tableModule, ...systemModule, ...fileModule, ...cateModule, ...testModule])
+  createProdMockServer([
+    ...userModule,
+    ...tableModule,
+    ...systemModule,
+    ...fileModule,
+    ...cateModule,
+    ...testModule,
+    ...areaModule
+  ])
 }
