e96cb3cce198caf0c8a47a1be3c00bc24b8b0f61
unknown <326010228@qq.com>
2023-11-12 14:19:20 +0800

    Update LayoutMix.vue

--- src/layout/LayoutMix.vue
+++ src/layout/LayoutMix.vue
@@ -8,18 +8,11 @@
         :auto-open-selected="false"
         @menu-item-click="onMenuItemClick"
       >
-        <a-menu-item v-for="item in menuRoutes" :key="item.path">
+        <a-menu-item v-for="item in topMenus" :key="item.path">
           <template #icon>
-            <GiSvgIcon
-              v-if="item?.meta?.svgIcon || item?.meta?.svgIcon || item.children?.[0]?.meta?.svgIcon"
-              :name="item?.meta?.svgIcon || item?.meta?.svgIcon || item.children?.[0]?.meta?.svgIcon"
-              :size="24"
-            ></GiSvgIcon>
+            <GiSvgIcon v-if="getMenuIcon(item, 'svgIcon')" :name="getMenuIcon(item, 'svgIcon')" :size="24"></GiSvgIcon>
             <template v-else>
-              <component
-                v-if="item?.meta?.icon || item?.meta?.icon || item.children?.[0]?.meta?.icon"
-                :is="item?.meta?.icon || item?.meta?.icon || item.children?.[0]?.meta?.icon"
-              ></component>
+              <component v-if="getMenuIcon(item, 'svgIcon')" :is="getMenuIcon(item, 'icon')"></component>
             </template>
           </template>
           <span>{{ item.meta?.title || item.children?.[0]?.meta?.title || '' }}</span>
@@ -29,7 +22,7 @@
     </header>
     <div class="layout-mix-wrapper">
       <section class="left">
-        <Menu :menus="childrenMenus" :menu-style="{ width: '200px', height: '100%' }"></Menu>
+        <Menu :menus="leftMenus" :menu-style="{ width: '180px', height: '100%' }"></Menu>
       </section>
       <section class="right">
         <Tabs></Tabs>
@@ -49,51 +42,56 @@ import { useRouteStore } from '@/stores'
 import type { RouteRecordRaw } from 'vue-router'
 import { isExternal } from '@/utils/validate'
 import { searchTree } from 'xe-utils'
+import { filterTree } from '@/utils'
 
 defineOptions({ name: 'LayoutMix' })
 const route = useRoute()
 const router = useRouter()
 const routeStore = useRouteStore()
+// 过滤是菜单的路由
+const menuRoutes = filterTree(routeStore.routes, (i) => i.meta?.hidden === false)
 
-const menuRoutes = ref<RouteRecordRaw[]>([])
-menuRoutes.value = JSON.parse(JSON.stringify(routeStore.routes))
-menuRoutes.value = menuRoutes.value.filter((i) => i.meta?.hidden === false)
-menuRoutes.value.forEach((item) => {
-  if (item.children && item.children.length) {
-    const showChildrens = item.children.filter((i) => i.meta?.hidden === false)
-    if (showChildrens.length === 1) {
-      // item = { ...item, meta: showChildrens[0].meta }
-      item.children = showChildrens
-    }
-  }
-})
-console.log('menuRoutes', toRaw(menuRoutes.value))
+// 顶部一级菜单
+const topMenus = ref<RouteRecordRaw[]>([])
+topMenus.value = JSON.parse(JSON.stringify(menuRoutes))
+console.log('topMenus', toRaw(topMenus.value))
+
+const getMenuIcon = (item: RouteRecordRaw, key: 'svgIcon' | 'icon') => {
+  return item.meta?.[key] || item.children?.[0].meta?.[key]
+}
 
 const onMenuItemClick = (key: string) => {
   if (isExternal(key)) {
     window.open(key)
-    return false
+    return
   }
-  getChildrenMenus(key)
-  const obj = menuRoutes.value.find((i) => i.path === key)
+  getLeftMenus(key)
+  const obj = topMenus.value.find((i) => i.path === key)
   if (obj && obj.redirect === 'noRedirect') return
   router.push({ path: key })
 }
 
+// 克隆是菜单的路由
+const cloneMenuRoutes: RouteRecordRaw[] = JSON.parse(JSON.stringify(menuRoutes))
+// 顶部一级菜单选中的
 const activeMenu = ref<string[]>([])
-const childrenMenus = ref<RouteRecordRaw[]>([])
-const getChildrenMenus = (key: string) => {
-  const arr = searchTree(routeStore.routes, (i) => i.path === key, { children: 'children' })
-  const rootPath = arr[0].path
-  const obj = routeStore.routes.find((i) => i.path === rootPath)
-  activeMenu.value = obj ? [obj.path] : []
-  childrenMenus.value = obj ? (obj.children as RouteRecordRaw[]) : []
+// 左侧的菜单
+const leftMenus = ref<RouteRecordRaw[]>([])
+// 获取左侧菜单
+const getLeftMenus = (key: string) => {
+  const arr = searchTree(cloneMenuRoutes, (i) => i.path === key, { children: 'children' })
+  const rootPath = arr.length ? arr[0].path : ''
+  const obj = cloneMenuRoutes.find((i) => i.path === rootPath)
+  activeMenu.value = obj ? [obj.path] : ['']
+  leftMenus.value = obj ? (obj.children as RouteRecordRaw[]) : []
 }
 
 watch(
   () => route.path,
   (newPath) => {
-    getChildrenMenus(newPath)
+    nextTick(() => {
+      getLeftMenus(newPath)
+    })
   },
   { immediate: true }
 )
@@ -102,6 +100,25 @@ watch(
 <style lang="scss" scoped>
 :deep(.arco-menu-pop) {
   white-space: nowrap;
+}
+
+:deep(.arco-menu.arco-menu-vertical.arco-menu-collapsed) {
+  // Menu菜单组件修改
+  .arco-menu-icon {
+    margin-right: 0;
+    padding: 10px 0;
+  }
+  .arco-menu-has-icon {
+    padding: 0;
+    justify-content: center;
+  }
+  .arco-menu-title {
+    display: none;
+  }
+}
+
+:deep(.arco-menu-horizontal .arco-menu-inner) {
+  padding-left: 0;
 }
 
 .layout-mix {
@@ -114,11 +131,11 @@ watch(
     overflow: hidden;
     display: flex;
     align-items: stretch;
-    .left {
+    > .left {
       border-right: 1px solid var(--color-border);
       background-color: var(--color-bg-1);
     }
-    .right {
+    > .right {
       flex: 1;
       display: flex;
       flex-direction: column;
