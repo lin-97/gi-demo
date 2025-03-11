f32a591ad709829e1956318bf701322a9c7cb93c
lin0716 <326010228@qq.com>
2025-03-11 18:11:15 +0800

    feat: 升级组件库版本，以及新增顶部布局（组件库menu组件缺陷，目前暂不完整）

---
+++ src/layout/LayoutTop.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="layout-top">
+    <a-row align="center" class="layout-top__header">
+      <Menu class="layout-top__menu"></Menu>
+      <HeaderRightBar></HeaderRightBar>
+    </a-row>
+    <Tabs></Tabs>
+    <Main></Main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Main from './components/Main.vue'
+import Tabs from './components/Tabs/index.vue'
+import Menu from './components/Menu/index.vue'
+import HeaderRightBar from './components/HeaderRightBar/index.vue'
+
+defineOptions({ name: 'LayoutTop' })
+</script>
+
+<style lang="scss" scoped>
+.layout-top {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+
+  &__header {
+    width: 100%;
+    overflow: hidden;
+    box-sizing: border-box;
+    border-bottom: 1px solid var(--color-border);
+    background-color: var(--color-bg-1);
+    padding-right: 16px;
+  }
+
+  &__menu {
+    flex: 1;
+    overflow: hidden;
+
+    :deep(.arco-menu-inner) {
+      padding-left: 0;
+    }
+  }
+}
+</style>
