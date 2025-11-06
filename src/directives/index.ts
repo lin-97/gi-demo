a844a229a3c467e768bb864b4eda17f4525c5131
木糖醇 <53759423+lin-97@users.noreply.github.com>
2025-11-06 14:31:22 +0800

    feat: 优化src下directives、hooks、stores的目录结构，更直观清晰

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,6 +1,6 @@
 import type { App } from 'vue'
-import hasPerm from './permission/hasPerm'
-import hasRole from './permission/hasRole'
+import hasPerm from './hasPerm'
+import hasRole from './hasRole'
 
 export default {
   install(Vue: App) {
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,11 +1,11 @@
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
-export * from './modules/app'
-export * from './modules/dict'
-export * from './modules/route'
-export * from './modules/tabs'
-export * from './modules/user'
+export * from './useAppStore'
+export * from './useDictStore'
+export * from './useRouteStore'
+export * from './useTabsStore'
+export * from './useUserStore'
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
