5dcaf463d63a051b6752fc15a45b9d64a7769266
木糖醇 <53759423+lin-97@users.noreply.github.com>
2026-01-07 18:31:25 +0800

    feat: 登录页重构

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,48 +1,53 @@
 <template>
   <div class="login">
-    <a-row align="stretch" class="login-box">
-      <a-col :xs="0" :sm="12" :md="15">
-        <div class="login-left">
-          <img class="login-left__img" src="@/assets/svgs/login-img.svg" />
+    <a-row align="stretch" class="w-full h-full">
+      <a-col :xs="0" :sm="0" :md="12" :lg="12" :xl="15" class="h-full">
+        <div class="login-left animate__animated animate__fadeInLeft">
+          <div class="login-left-name">
+            <h3 class="system-name">Gi Admin Pro</h3>
+            <!-- <p>美观实用的界面，经过视觉优化，确保卓越的用户体验</p> -->
+          </div>
         </div>
       </a-col>
-      <a-col :xs="24" :sm="12" :md="9">
-        <div class="login-right">
-          <a-form ref="formRef" :size="isMobile() ? 'large' : 'medium'" :model="form" :rules="rules"
-            :style="{ width: '84%' }" :label-col-style="{ display: 'none' }" :wrapper-col-style="{ flex: 1 }">
-            <h3 class="login-right__title">
-              <img class="logo" src="@/assets/images/logo.gif" /><span>Admin Pro</span>
-            </h3>
-            <a-form-item field="username">
-              <a-input v-model="form.username" placeholder="账号 admin/user" allow-clear>
-                <template #prefix><icon-user :stroke-width="1" :style="{ fontSize: '20px' }" /></template>
-              </a-input>
-            </a-form-item>
-            <a-form-item field="password">
-              <a-input-password v-model="form.password" placeholder="密码" allow-clear>
-                <template #prefix><icon-lock :stroke-width="1" :style="{ fontSize: '20px' }" /></template>
-              </a-input-password>
-            </a-form-item>
-            <a-form-item>
-              <a-row justify="space-between" align="center" class="w-full">
-                <a-checkbox v-model="checked">记住密码</a-checkbox>
-                <a-link>忘记密码</a-link>
-              </a-row>
-            </a-form-item>
-            <a-form-item>
-              <a-space direction="vertical" fill class="w-full">
-                <a-button type="primary" size="large" long :loading="loading" @click="login">登录</a-button>
-                <a-button type="text" size="large" long class="register-btn">注册账号</a-button>
-              </a-space>
-            </a-form-item>
-          </a-form>
+      <a-col :xs="24" :sm="24" :md="12" :lg="12" :xl="9" class="h-full">
+        <div class="login-box animate__animated animate__fadeInRight">
+          <GiThemeBtn class="theme-btn"></GiThemeBtn>
+          <div class="login-box-content">
+            <h3 class="title">欢迎回来</h3>
+            <p class="sub-title">输入您的账号和密码登录</p>
+            <a-form ref="formRef" size="large" :model="form" :rules="rules" layout="vertical"
+              :label-col-props="{ style: { marginBottom: '0' } }">
+              <a-form-item field="username" label="账号">
+                <a-input v-model="form.username" placeholder="账号 admin/user" allow-clear>
+                  <template #prefix><icon-user :stroke-width="3" :style="{ fontSize: '20px' }" /></template>
+                </a-input>
+              </a-form-item>
+              <a-form-item field="password" label="密码">
+                <a-input-password v-model="form.password" placeholder="密码" allow-clear>
+                  <template #prefix><icon-lock :stroke-width="3" :style="{ fontSize: '20px' }" /></template>
+                </a-input-password>
+              </a-form-item>
+              <a-form-item>
+                <a-row justify="space-between" align="center" class="w-full">
+                  <a-checkbox v-model="checked">记住密码</a-checkbox>
+                  <a-link>忘记密码</a-link>
+                </a-row>
+              </a-form-item>
+              <a-form-item>
+                <a-space direction="vertical" fill class="w-full">
+                  <a-button type="primary" size="large" long :loading="loading" @click="login">
+                    登录
+                  </a-button>
+                  <a-button type="text" size="large" long class="register-btn">
+                    注册账号
+                  </a-button>
+                </a-space>
+              </a-form-item>
+            </a-form>
+          </div>
         </div>
       </a-col>
     </a-row>
-
-    <GiThemeBtn class="theme-btn"></GiThemeBtn>
-
-    <LoginBg></LoginBg>
   </div>
 </template>
 
@@ -51,9 +56,7 @@ import type { FormInstance } from '@arco-design/web-vue'
 import { Message } from '@arco-design/web-vue'
 import { useLoading } from '@/hooks'
 import { useTabsStore, useUserStore } from '@/stores'
-import { isMobile } from '@/utils'
 import * as Regexp from '@/utils/regexp'
-import LoginBg from './components/LoginBg/index.vue'
 
 defineOptions({ name: 'Login' })
 const router = useRouter()
@@ -73,13 +76,12 @@ const rules: FormInstance['rules'] = {
   ]
 }
 
-// 记住密码
-const checked = ref(false)
+const formRef = useTemplateRef('formRef')
 // 登录加载
 const { loading, setLoading } = useLoading()
-const errorMessage = ref('')
+// 记住密码
+const checked = ref(false)
 
-const formRef = useTemplateRef('formRef')
 // 点击登录
 const login = async () => {
   try {
@@ -97,7 +99,7 @@ const login = async () => {
     })
     Message.success('登录成功')
   } catch (error) {
-    errorMessage.value = (error as Error).message
+    Message.error((error as Error).message)
   } finally {
     setLoading(false)
   }
@@ -105,68 +107,73 @@ const login = async () => {
 </script>
 
 <style lang="scss" scoped>
+:deep(.arco-input-wrapper),
+:deep(.arco-btn-size-large) {
+  height: 40px;
+}
+
 .login {
   display: flex;
   align-items: center;
   justify-content: center;
   height: 100%;
   background-color: var(--color-bg-5);
-
-  &-box {
-    z-index: 999;
-    display: flex;
-    width: 86%;
-    max-width: 720px;
-    height: 380px;
-    box-shadow: 0 2px 4px 2px rgb(0 0 0 / 8%);
-  }
 }
 
 .login-left {
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   width: 100%;
   height: 100%;
-  overflow: hidden;
-  background: linear-gradient(60deg, rgb(var(--primary-6)), rgb(var(--primary-3)));
+  background-image: url('@/assets/images/login-bg.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: relative;
 
-  &__img {
+  &-name {
     position: absolute;
-    inset: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    transition: all 0.3s;
+    top: 20px;
+    left: 30px;
+    color: #666;
+  }
+
+  .system-name {
+    font-size: 24px;
+    font-weight: 700;
+    line-height: 1.5;
+    color: #212121;
   }
 }
 
-.login-right {
+.login-box {
+  height: 100%;
   box-sizing: border-box;
   display: flex;
-  align-items: center;
+  flex-direction: column;
   justify-content: center;
-  width: 100%;
-  height: 100%;
+  align-items: center;
   padding-top: 30px;
   background: var(--color-bg-1);
+  position: relative;
 
-  &__title {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-bottom: 20px;
-    font-size: 20px;
-    font-weight: 500;
-    line-height: 32px;
-    color: var(--color-text-1);
-    text-align: center;
+  &-content {
+    width: 100%;
+    max-width: 450px;
+    padding: 20px;
+    box-sizing: border-box;
 
-    .logo {
-      width: 32px;
-      height: 32px;
-      margin-right: 6px;
+    .title {
+      font-size: 36px;
+      font-weight: 700;
+      line-height: 1.2;
+      color: var(--color-text-1);
+      margin-bottom: 12px;
+    }
+
+    .sub-title {
+      font-size: 14px;
+      line-height: 20px;
+      color: var(--color-text-3);
+      margin-bottom: 12px;
     }
   }
 }
@@ -177,9 +184,9 @@ const login = async () => {
 }
 
 .theme-btn {
-  position: fixed;
+  position: absolute;
   top: 20px;
-  left: 30px;
+  right: 30px;
   z-index: 9999;
 }
 </style>
